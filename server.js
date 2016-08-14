7a994993cb9f78de1f1f07f41576558de1020862
Ola Adolfsson <ola.adolfsson@ist.com>
2016-08-14 15:38:30 +0200

    Added  suppprt fpr x-origin

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ hermsGpio.setup();
 // API routes
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/pins', (req, res) => {
   let pins = hermsGpio.getPins();
   let pinToReturn = [];
@@ -49,21 +55,27 @@ app.get('/api/pins/:name', (req, res) => {
         console.log(error);
         res.status(500).send({ 'error': error.message });
       } else {
-        res.send({ 'pin': pinName, 'value': value });
+        res.send({ 'name': pinName, 'value': value });
       }
     });
   }
 });
 
 app.post('/api/pins/:name', (req, res) => {
-  if (!req.params.name) {
+  let name = req.params.name;
+  let value = req.params.value;
+
+  if (!name) {
     res.status(400).send('Missing parameter: name');
-  } else if (req.body.value === undefined) {
+  } else if (value === 'undefined') {
     res.status(400).send({ "error": "Missing pin value" });
+  } else if (!isParamValueValid(value)) {
+    res.status(400).send({ "error": "value must be true or false" })
   } else {
 
     let pinName = req.params.name;
     let value = req.body.value;
+    value = (value === 'false' || value === false ? 0 : 1);
 
     hermsGpio.writePin(pinName, value, (err) => {
       if (err) {
@@ -75,7 +87,12 @@ app.post('/api/pins/:name', (req, res) => {
   }
 });
 
-
+function isParamValueValid(value) {
+  return !(value === true ||
+    value === false ||
+    value === 'true' ||
+    value === 'false');
+}
 app.listen(8081, function () {
   logger.logInfo("server", "Server started on 8081");
 });
