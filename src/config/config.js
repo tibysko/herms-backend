624c92381efa6b569265161e7ea9e2814fac969c
Ola Adolfsson <Ola.Adolfsson@ist.com>
2016-11-16 23:19:48 +0100

    changes port variable

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    port: process.env.PORT | 8080,
+    port: process.env.port | 8080,
     websocketPort: process.env.SOCKET_PORT | 8081,
     usbPort: process.env.USB_PORT | 'com3'
 }
