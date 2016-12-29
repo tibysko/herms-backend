9b7af1b929be705ce0a7e0fbe873efec6886d3e7
Martin Berglund <martin.berglund@cgv.se>
2016-12-29 15:39:20 +0100

    Added parameter mashwater Added case for filling up MLT

--- src/valve/valve-controller-mlt-hw-in.js
+++ src/valve/valve-controller-mlt-hw-in.js
@@ -3,6 +3,7 @@ const parameterController = require('../parameters/parameter-controller');
 
 const L1_OFFSET = 'l1_offset';
 const L1_SCALING = 'l1_scaling';
+const MASHWATER = 'MashWater';
 const SPARGEWATER = 'SpargeWater';
 
 class levelControllerHlt {
@@ -14,11 +15,13 @@ class levelControllerHlt {
     this.actLevelHlt = 0;
     this.levelScaling = 1;
     this.levelOffset = 0;
-    this.start = false; //Todo fix startbutton from gui
+    this.activateSparge = false; //Todo fix startbutton from gui
+    this.activateDoughIn = false; //Todo fix button from gui
     this.auto = false; //Todo fix auto from gui
     this.startLevel = 0;
     this.valveClosed = 0;
-    this.spargeActive = false;
+    this.spargeActive = false; //Todo show in gui
+    this.doughInActive = false; //Todo show in gui
     this.stateValve = 0;
 
     parameterController.on('data', (data) => {
@@ -45,6 +48,7 @@ class levelControllerHlt {
       // Manual mode
       case 0:
         this.spargeActive = false;
+        this.doughInActive = false;
 
         if (this.auto) {
           this.stateValve = 100;
@@ -52,17 +56,47 @@ class levelControllerHlt {
         }
 
       // Auto wait for start sparge
-      case 100:
-        if (this.start) {
+      case 10:
+        // Dough in activated
+        if (this.activateMashWater) {
+          this.valveController.setState('MLT_HW_IN', ValveConstants.START_OPEN, function () { });
+          this.startLevel = this.actLevelHlt;
+          this.doughInActive = true;
+          this.stateValve = 100;
+          break;
+          // Sparge activated
+        } else if (this.activateSparge) {
           this.valveController.setState('MLT_HW_IN', ValveConstants.START_OPEN, function () { });
           this.startLevel = this.actLevelHlt;
           this.spargeActive = true;
+          this.stateValve = 200;
+          break;
+        }
+
+
+      // Dough in active
+      case 100:
+        // Go back to Manual mode
+        if (!this.auto) {
+          this.stateValve = 0;
+          break;
+          // Sparge done
+        } else if ((this.startLevel - this.actLevelHlt) <= MASHWATER && this.startLevel > this.actLevelHlt) {
+          this.valveController.setState('MLT_HW_IN', ValveConstants.START_CLOSE, function () { });
+          this.spargeActive = false;
           this.stateValve = 110;
           break;
         }
 
-      // Sparge active
+      // Set alarm for sparge done
       case 110:
+        // Todo Set alarm for sparge done
+        this.stateValve = 0;
+        break;
+
+
+      // Sparge active
+      case 200:
         // Go back to Manual mode
         if (!this.auto) {
           this.stateValve = 0;
@@ -71,12 +105,12 @@ class levelControllerHlt {
         } else if ((this.startLevel - this.actLevelHlt) <= SPARGEWATER && this.startLevel > this.actLevelHlt) {
           this.valveController.setState('MLT_HW_IN', ValveConstants.START_CLOSE, function () { });
           this.spargeActive = false;
-          this.stateValve = 120;
+          this.stateValve = 210;
           break;
         }
 
       // Set alarm for sparge done
-      case 120:
+      case 210:
         // Todo Set alarm for sparge done
         this.stateValve = 0;
         break;
