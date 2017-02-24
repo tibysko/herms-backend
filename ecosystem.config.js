d2aee552d837dd76b75b47e9c0c6ac7915bb6e19
Ola Adolfsson <Ola.Adolfsson@ist.com>
2017-02-24 20:03:54 +0100

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,17 @@ module.exports = {
     {
       name: "Backend",
       script: "src/server.js"
+      env: {
+        NODE_ENV: "production",
+        USB_PORT: "/dev/ttyACM0",
+        BACKEND_PORT: 8081,
+        BACKEND_WEBSOCKET_PORT: 8082
+
+      },
+
+      env_aws: {
+        MOCK: "true"
+      }
     }
   ],
 
@@ -24,14 +35,7 @@ module.exports = {
       repo: "https://github.com/tibysko/herms-backend.git",
       path: "/home/pi/herms/backend",
       exec_mode: 'fork',
-      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env production",
-      env: {
-        NODE_ENV: "production",
-        USB_PORT: "/dev/ttyACM0",
-        BACKEND_PORT: 8081,
-        BACKEND_WEBSOCKET_PORT: 8082
-
-      }
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env production"
     },
     aws: {
       user: "ubuntu",
@@ -41,14 +45,7 @@ module.exports = {
       repo: "https://github.com/tibysko/herms-backend.git",
       path: "/home/ubuntu/herms/backend",
       exec_mode: 'fork',
-      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env production",
-      env: {
-        NODE_ENV: "production",
-        BACKEND_PORT: 8081,
-        BACKEND_WEBSOCKET_PORT: 8082,
-        "OLA": "SANT",
-        MOCK: "true"
-      }
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env aws"
     }
   }
 }
