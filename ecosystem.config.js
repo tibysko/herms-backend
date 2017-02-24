8fb34ebe7931889f8e5cd97f26b73971a9633534
Ola Adolfsson <Ola.Adolfsson@ist.com>
2017-02-24 20:11:46 +0100

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
+
   "apps": [
 
-    // First application
+
     {
       "name": "Backend",
       "script": "src/server.js"
@@ -22,10 +19,7 @@ module.exports = {
     }
   ],
 
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
+
   "deploy": {
     "production": {
       "user": "pi",
