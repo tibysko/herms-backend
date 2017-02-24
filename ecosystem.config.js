b3dae3a338f341ae58c2610ee39a4e81cd57ccbb
Ola Adolfsson <Ola.Adolfsson@ist.com>
2017-02-24 20:10:46 +0100

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,22 +3,21 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps: [
+  "apps": [
 
     // First application
     {
-      name: "Backend",
-      script: "src/server.js"
-      env: {
-        NODE_ENV: "production",
-        USB_PORT: "/dev/ttyACM0",
-        BACKEND_PORT: 8081,
-        BACKEND_WEBSOCKET_PORT: 8082
-
+      "name": "Backend",
+      "script": "src/server.js"
+      "env": {
+        "NODE_ENV": "production",
+        "USB_PORT": "/dev/ttyACM0",
+        "BACKEND_PORT": "8081",
+        "BACKEND_WEBSOCKET_PORT": "8082"
       },
 
-      env_aws: {
-        MOCK: "true"
+      "env_aws": {
+        "MOCK": "true"
       }
     }
   ],
@@ -27,24 +26,24 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy: {
-    production: {
-      user: "pi",
-      host: "192.168.1.146",
-      ref: "origin/master",
-      repo: "https://github.com/tibysko/herms-backend.git",
-      path: "/home/pi/herms/backend",
-      exec_mode: 'fork',
+  "deploy": {
+    "production: {
+      "user": "pi",
+      "host": "192.168.1.146",
+      "ref": "origin/master",
+      "repo": "https://github.com/tibysko/herms-backend.git",
+      "path": "/home/pi/herms/backend",
+      "exec_mode": "fork",
       "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env production"
     },
-    aws: {
-      user: "ubuntu",
-      host: "52.11.194.229",
-      port: "22",
-      ref: "origin/master",
-      repo: "https://github.com/tibysko/herms-backend.git",
-      path: "/home/ubuntu/herms/backend",
-      exec_mode: 'fork',
+    "aws": {
+      "user": "ubuntu",
+      "host": "52.11.194.229",
+      "port": "22",
+      "ref": "origin/master",
+      "repo": "https://github.com/tibysko/herms-backend.git",
+      "path": "/home/ubuntu/herms/backend",
+      "exec_mode": 'fork',
       "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env aws"
     }
   }
